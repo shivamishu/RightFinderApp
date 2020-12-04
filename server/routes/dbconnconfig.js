346e38acbacf26f8f09d90879ef556c3391081ee
nayak-codebase <praveen.nayak86@gmail.com>
2020-12-05 02:21:25 +0530

    Update dbconnconfig.js
    db config change

--- server/routes/dbconnconfig.js
+++ server/routes/dbconnconfig.js
@@ -1,13 +1,6 @@
 "use strict";
 var mysql = require("mysql");
-// var connection = mysql.createConnection({
-//   host: process.env.AWS_RDS_ENDPOINT,
-//   user: process.env.AWS_RDS_USERNAME,
-//   password: process.env.AWS_RDS_PASSWORD,
-//   port: process.env.AWS_RDS_PORT,
-//   database: process.env.AWS_RDS_DATABASE,
-//   multipleStatements: true,
-// });
+
 var pool  = mysql.createPool({
   connectionLimit : 10,
   host            : process.env.AWS_RDS_ENDPOINT,
@@ -16,5 +9,4 @@ var pool  = mysql.createPool({
   database: process.env.AWS_RDS_DATABASE,
   multipleStatements: true,
 });
-// module.exports = connection;
 module.exports = pool;
